3e908b1a6d7438e8a3682f45dee64f61d51621e1
wzolleis <wzolleis@web.de>
2018-03-28 00:43:39 +0200

    Navbar anzeigen (nur Beispiel)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import './App.css';
 import { TeamList } from './team/TeamListContainer';
+import { Navbar } from './navbar/Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header>
           <h1 className="App-title">Coffee Cleaner</h1>
-         
+            <Navbar/>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
